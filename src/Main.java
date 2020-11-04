b3a9f3ab9163c86243790c5f49766b289acbd52c
Dina Orlova <dina.turilova@gmail.com>
2020-11-04 10:26:14 +0300

    add Main.java

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceTicketKopecks = 457300;
+        int bonusMiles = priceTicketKopecks/20/100;
+                System.out.println(bonusMiles);
+    }
+}
